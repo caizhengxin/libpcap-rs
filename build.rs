a6c862a7ff71338e4d87dda4cd1b96c91a195024
jankincai <jankincai12@gmail.com>
2024-03-15 13:53:39 +0800

    :hammer: refactor(upgrade): update `LICENSE、Cargo.toml`.

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     // Tell cargo to look for shared libraries in the specified directory
     println!("cargo:rustc-link-search=/usr/lib");
 
-    // Tell cargo to tell rustc to link the system bzip2
+    // Tell cargo to tell rustc to link the system pcap
     // shared library.
     println!("cargo:rustc-link-lib=pcap");
 
